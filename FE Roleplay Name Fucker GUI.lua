173c7cc4e5506944015ce7b2f4b2c7cb4459723c
system32unknown <79248736+system32unknown@users.noreply.github.com>
2022-06-26 00:56:33 +0900

    update

--- FE Roleplay Name Fucker GUI.lua
+++ FE Roleplay Name Fucker GUI.lua
@@ -180,6 +180,7 @@ end)
 Section1:CreateTextBox("TextArea", "hello world!", false, function(String)
 	texts = String
 end)
+Section1:SetValue("lorum ipsum")
 
 Section1:CreateButton("Inject Name", function()
 	Change_All_Name(texts, islocalplayer, Selected_Player)
