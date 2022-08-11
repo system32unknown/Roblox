588321548399e40f5818cb36f9816803bd302c1b
system32unknown <op58274@gmail.com>
2022-08-11 17:21:41 +0900

    messag

---
+++ MakeMessageClient.lua
@@ -0,0 +1,13 @@
+function FireMessageClient(text, color, font, size)
+    local text = text or "Hello World"
+    local color = color or Color3.fromRGB(255, 255, 255)
+    local font = font or Enum.Font.SourceSansBold
+    local size = size or 18
+
+    game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage", {
+        Text = text;
+        Color = color;
+        Font = font;
+        TextSize = size;
+    })
+end
