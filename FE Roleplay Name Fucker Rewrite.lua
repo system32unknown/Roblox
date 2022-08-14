9cce4d0f4fd068e5318bf342992f4772701bedc8
system32unknown <op58274@gmail.com>
2022-08-14 23:36:47 +0900

    massive update

--- FE Roleplay Name Fucker Rewrite.lua
+++ FE Roleplay Name Fucker Rewrite.lua
@@ -1,5 +1,5 @@
 for _, object in pairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
-    if not (object.Name == "ServerHandler" and object:IsA("RemoteEvent")) then
+    if not object.Name == "ServerHandler" and object:IsA("RemoteEvent") then
         warn("RemoteEvent 'ServerHandler' not found!")
         return
     end
@@ -92,26 +92,22 @@ function PlayerManager.Get_Player_Arrays(array:table)
 	table.sort(array)
 end
 
-function CheckColor(Color: table | Color3)
-	if type(Color) == "table" then
-		return Color3.fromRGB(Color[1], Color[2], Color[3])
-	elseif typeof(Color) == "Color3" then
-		return Color
-	end
-end
-
 local prev_name = PlayerManager.Get_Name("LocalPlayer")
-local texts, review_text = "", ""
-local IsAll, IsLocal = false, false
+local texts, Selected_Player = "", ""
+local IsLocal, looped = false, false
 local waits, Loops = 0, 0
 local minBytes, maxBytes = 0, 255
-
-local Toggled, looped = true, false
-local Selected_Player = ""
+local del_num = 0
 
 local event_text, wait_text = "", ""
-local Text_List, Wait_List = {}, {}
+local Text_List, Wait_List = {"Hello World!"}, {1}
 local Players_List = {}
+
+function UpdateName(Labels)
+    prev_name = PlayerManager.Get_Name("LocalPlayer")
+    Labels[1]:SetText("My Name: " .. texts)
+    Labels[2]:SetText("Prev Name: " .. prev_name)
+end
 
 PlayerManager.Get_Player_Arrays(Players_List)
 
@@ -119,19 +115,16 @@ local Window = Library:CreateWindow({Title = '[FE] Roleplay Name Fucker by Frisk
 	local Tabs = {Main = Window:AddTab('Main'), ['UI Settings'] = Window:AddTab('UI Settings')} do
 		local Tab = Tabs.Main:AddLeftTabbox('Main') do
 	    	local MainTab = Tab:AddTab('Main') do
-                local LocalName = IsLocal and PlayerManager.Get_Name("LocalPlayer") or PlayerManager.Get_Name(Selected_Player)
                 local Label = MainTab:AddLabel("My Name: " .. (texts ~= "" and texts or "No Name"))
-				local PrevLabel = MainTab:AddLabel("Prev Name: " .. (LocalName ~= "" and LocalName or "No Name"))
+				local PrevLabel = MainTab:AddLabel("Prev Name: " .. (prev_name ~= "" and prev_name or "No Name"))
 
 	    	    local InjectButton = MainTab:AddButton('Inject', function()
-					if LocalName ~= "" then
-						Label:SetText("My Name: " .. texts)
-						PrevLabel:SetText("Prev Name: " .. LocalName)
-                    	PlayerManager.Change_All_Name(texts, IsAll, Selected_Player)
+					if PlayerManager.Get_Name("LocalPlayer") ~= "" then
+                        UpdateName({Label, PrevLabel})
+                    	PlayerManager.Change_All_Name(texts, IsLocal, Selected_Player)
                     	while looped do
-							Label:SetText("My Name: " .. texts)
-							PrevLabel:SetText("Prev Name: " .. LocalName)
-                    	    PlayerManager.Change_All_Name(texts, IsAll, Selected_Player)
+                            UpdateName({Label, PrevLabel})
+                    	    PlayerManager.Change_All_Name(texts, IsLocal, Selected_Player)
                     	    wait_Func(waits)
                     	end
 					end
@@ -143,20 +136,49 @@ local Window = Library:CreateWindow({Title = '[FE] Roleplay Name Fucker by Frisk
                     PlayerManager.Change_All_Name(prev_name, true)
 	    	    end)
 
-	    	    MainTab:AddSlider('MinSlider', {Text = 'Min', Default = 0, Min = 0, Max = 255, Rounding = 1, Compact = false})
+                MainTab:AddDivider()
+
+	    	    MainTab:AddSlider('MinSlider', {Text = 'Min', Default = 0, Min = 0, Max = 255, Rounding = 0, Compact = false})
 	    	    Options.MinSlider:OnChanged(function()
 	    	        minBytes = Options.MinSlider.Value
 	    	    end)
 
-	    	    MainTab:AddSlider('MaxSlider', {Text = 'Max', Default = 0, Min = 0, Max = 255, Rounding = 1, Compact = false})
+	    	    MainTab:AddSlider('MaxSlider', {Text = 'Max', Default = 0, Min = 0, Max = 255, Rounding = 0, Compact = false})
 	    	    Options.MaxSlider:OnChanged(function()
 	    	        minBytes = Options.MaxSlider.Value
 	    	    end)
 
-	    	    MainTab:AddInput('MyTextbox', {Default = '', Numeric = false, Finished = false, Placeholder = 'Name'})
-	    	    Options.MyTextbox:OnChanged(function()
-	    	        texts = Options.MyTextbox.Value
+	    	    MainTab:AddInput('NameTextbox', {Default = '', Numeric = false, Finished = false, Text = 'RP Name:', Placeholder = 'Name'})
+	    	    Options.NameTextbox:OnChanged(function()
+	    	        texts = Options.NameTextbox.Value
 	    	    end)
+
+                MainTab:AddButton("Inject Name then Exit", function()
+                    PlayerManager.Change_All_Name(texts, IsLocal, Selected_Player)
+                    wait_Func(0.5)
+                    game:shutdown()
+                end)
+
+                MainTab:AddButton("TypeWriter", function()
+                    TextManager.TypeWrite(texts, waits, IsLocal, Selected_Player)
+                end)
+
+                MainTab:AddButton("Revert Name", function()
+                    UpdateName({Label, PrevLabel})
+                    PlayerManager.Change_All_Name(prev_name, true)
+                end)
+
+                MainTab:AddButton("Randomize Name", function()
+                    PlayerManager.Change_All_Name(TextManager.Random_Text(Loops, minBytes, maxBytes), IsLocal)
+                end)
+
+                MainTab:AddButton("Shuffle Name", function()
+                    for _, object in pairs((IsLocal and game:GetService("Players")[Selected_Player].Character:GetDescendants() or workspace:GetDescendants())) do
+                        if object.Name == "ServerHandler" and object:IsA("RemoteEvent") then
+                            object:FireServer(TextManager.shuffle(texts))
+                        end
+                    end
+                end)
 	    	end
 
 			local section = Tab:AddTab('Extras') do
@@ -170,20 +192,24 @@ local Window = Library:CreateWindow({Title = '[FE] Roleplay Name Fucker by Frisk
 	    	        wait_Func = Toggles.UseLegacy.Value and wait or task.wait
 	    	    end)
 
-	    	    section:AddToggle('UseAll', {Text = 'Is All', Tooltip = 'Uses All Players'})
+	    	    section:AddToggle('UseAll', {Text = 'Is Local', Tooltip = 'Uses Local Players'})
 	    	    Toggles.UseAll:OnChanged(function()
-	    	        IsAll = Toggles.UseAll.Value
+	    	        IsLocal = Toggles.UseAll.Value
 	    	    end)
-
-	    	    section:AddToggle('UseLocal', {Text = 'Is Local', Tooltip = 'Uses Local Player'})
-	    	    Toggles.UseLocal:OnChanged(function()
-	    	        IsLocal = Toggles.UseLocal.Value
-	    	    end)
-                Toggles.UseLocal:SetValue(true)
 
 	    	    local dropdown = section:AddDropdown('PlayerDropDown', {Values = Players_List, Default = 1, Multi = false, Text = 'Player List'})
 	    	    Options.PlayerDropDown:OnChanged(function()
 	    	        Selected_Player = Options.PlayerDropDown.Value
+	    	    end)
+
+	    	    section:AddInput('WaitBox', {Default = '1', Numeric = true, Finished = false, Name = 'Wait Numbers', Placeholder = 'Wait Numbers'})
+	    	    Options.WaitBox:OnChanged(function()
+	    	        waits = Options.WaitBox.Value
+	    	    end)
+
+	    	    section:AddInput('LoopBox', {Default = '1', Numeric = true, Finished = false, Name = 'Loop Numbers', Placeholder = 'Loop Numbers'})
+	    	    Options.LoopBox:OnChanged(function()
+	    	        Loops = Options.LoopBox.Value
 	    	    end)
 
 	    	    section:AddButton('Update Player List', function()
@@ -191,8 +217,102 @@ local Window = Library:CreateWindow({Title = '[FE] Roleplay Name Fucker by Frisk
                     PlayerManager.Get_Player_Arrays(Players_List)
                     dropdown:SetValues(Players_List)
 	    	    end)
+
+                section:AddButton('Grab RP Name', function()
+                    local getting_name = PlayerManager.Get_Name(Selected_Player)
+                    if Selected_Player ~= "" or PlayerManager.Get_Name(Selected_Player) ~= "" then
+                        MessageLIB:Notification("Grabbed Name!", "Copied to Clipboard!", 2, 4914902918)
+                        setclipboard(getting_name)
+                    end
+                end)
+
+                section:AddButton("Revert Selected Player", function()
+                    Selected_Player = ""
+                end)
 			end
 		end
+
+        local Tab = Tabs.Main:AddRightTabbox('Events') do
+            local MainTab = Tab:AddTab('Main') do
+                local EventLabel = MainTab:AddLabel("Text: ")
+
+                event_list = MainTab:AddDropdown('EventDropdown', {Values = Text_List, Default = 1, Multi = false, Text = 'Event Text List'})
+                Options.EventDropdown:OnChanged(function()
+                    EventLabel:SetText("Text: " .. Options.EventDropdown.Value)
+                end)
+
+                wait_lists = MainTab:AddDropdown('WaitDropdown', {Values = Wait_List, Default = 1, Multi = false, Text = 'Event Wait List'})
+
+	    	    MainTab:AddInput('EventTextBox', {Numeric = false, Finished = false, Text = 'Event Text', Placeholder = 'Event Text'})
+	    	    Options.EventTextBox:OnChanged(function()
+	    	        event_text = Options.EventTextBox.Value
+	    	    end)
+
+	    	    MainTab:AddInput('EventWaitBox', {Numeric = true, Finished = false, Text = 'Event Wait', Placeholder = 'Event Wait'})
+	    	    Options.EventWaitBox:OnChanged(function()
+	    	        wait_text = Options.EventWaitBox.Value
+	    	    end)
+
+                MainTab:AddButton("Add Lists", function()
+                    if event_text ~= "" then
+                        table.insert(Text_List, event_text)
+                        event_list:SetValues(Text_List)
+                    end
+
+                    if wait_text ~= "" then
+                        table.insert(Wait_List, tonumber(wait_text))
+                        wait_lists:SetValues(Wait_List)
+                    end
+                end)
+
+                MainTab:AddButton("Inject List", function()
+                    for i, v in pairs(Text_List) do
+                        PlayerManager.Change_All_Name(v, IsLocal, Selected_Player)
+                        wait_Func(Wait_List[i])
+                    end
+                end)
+
+                MainTab:AddButton("Inject List Randomly", function()
+                    local choiced_name = Text_List[math.random(0, #Text_List)]
+                    PlayerManager.Change_All_Name(choiced_name, IsLocal, Selected_Player)
+                end)
+
+                MainTab:AddButton("TypeWrite List", function()
+                    for i, v in pairs(Text_List) do
+                        TextManager.TypeWrite(v, waits, IsLocal, Selected_Player)
+                        wait_Func(Wait_List[i])
+                    end
+                end)
+            end
+
+            local SettingTab = Tab:AddTab('List Settings') do
+                SettingTab:AddButton("Delete All Lists", function()
+                    if #Text_List > 0 then
+                        table.clear(Text_List)
+                        event_list:SetValues(Text_List)
+					end
+					if #Wait_List > 0 then
+                        table.clear(Wait_List)
+                        wait_lists:SetValues(Wait_List)
+                    end
+                end)
+
+	    	    SettingTab:AddInput('DelListBox', {Numeric = true, Finished = false, Text = 'Delete List Number'})
+	    	    Options.DelListBox:OnChanged(function()
+	    	        del_num = tonumber(Options.DelListBox.Value)
+	    	    end)
+
+                SettingTab:AddButton("Delete List", function()
+                    if #Text_List > 0 or #Wait_List > 0 then
+                        table.remove(Text_List, del_num)
+                        table.remove(Wait_List, del_num)
+
+                        event_list:SetValues(Text_List)
+                        wait_lists:SetValues(Wait_List)
+                    end
+                end)
+            end
+        end
 
 		Library:OnUnload(function()
 		    Library.Unloaded = true
@@ -211,7 +331,7 @@ local Window = Library:CreateWindow({Title = '[FE] Roleplay Name Fucker by Frisk
 		ThemeManager:SetLibrary(Library)
 		SaveManager:SetLibrary(Library)
 
-		SaveManager:IgnoreThemeSettings() 
+		SaveManager:IgnoreThemeSettings()
 		SaveManager:SetIgnoreIndexes({'MenuKeybind'})
 
 		ThemeManager:SetFolder('MyScriptHub')
@@ -221,3 +341,6 @@ local Window = Library:CreateWindow({Title = '[FE] Roleplay Name Fucker by Frisk
 		ThemeManager:ApplyToTab(Tabs['UI Settings'])
 	end
 end
+
+Library:Notify('Script fully loaded!')
+Library:Notify('Press '.. Library.ToggleKeybind.Value ..' to toggle the menu!')
