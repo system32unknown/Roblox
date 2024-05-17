13392d7ff3331e6ee24628638a9214611683cee4
Altertoriel <79248736+system32unknown@users.noreply.github.com>
2024-05-18 00:02:42 +0900

    Update SoundFucker.lua

--- SoundFucker.lua
+++ SoundFucker.lua
@@ -1,8 +1,8 @@
-local SongList = {3929730934, 5567523008, 5517133180, 2631687985, 2478816735, 188088048, 343430735, 6343741731, 8280196339, 5538547181, 7696427240}
+local SongList = {3929730934, 5567523008, 2631687985, 188088048, 343430735, 6343741731, 5538547181, 7696427240, 7696491068, 7145372503, 7147215136, 3900067524, 3175432527, 8294288087, 6201427049}
 
 function setSound(pitch, Volume, SoundID, Remote, looped)
     looped = looped or true
-	local randomized_id = "V3rm was here".. math.random(5838327)
+	local randomized_id = "HACK_".. math.random(5838327) .. "_PROTECTED" .. math.random(5838327)
    	local Args = {
 		[1] = "newSound",
 		[2] = randomized_id,
@@ -16,7 +16,7 @@ function setSound(pitch, Volume, SoundID, Remote, looped)
 	return {[1] = "playSound", [2] = randomized_id}
 end
 
-for _, v in pairs(workspace:GetDescendants()) do
+for _, v in pairs(game:GetDescendants()) do
     if v.Name == "AC6_FE_Sounds" then
         local soundshit = SongList[math.random(1, #SongList)]
         v:FireServer(unpack(setSound(math.random(-3, 3), 255, soundshit, v, true)))
