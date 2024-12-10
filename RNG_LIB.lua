7518069a6ac663543210a837195fb065f3f059be
Altertoriel <79248736+system32unknown@users.noreply.github.com>
2024-12-10 11:29:36 +0900

    Add files via upload

---
+++ RNG_LIB.lua
@@ -0,0 +1,28 @@
+local RNG = {}
+RNG.__index = RNG
+
+function RNG.char(valueRange, length):string
+    local result = ""
+    for _ = 1, length do
+        result = result .. utf8.char(math.random(0, valueRange))
+    end
+    return result
+end
+
+function RNG.vector(n:number):Vector3
+    return Vector3.new(math.random(-n, n), math.random(-n, n), math.random(-n, n)) 
+end
+
+function RNG.CFrame(positionRange, rotationRange):CFrame
+	local randomX = math.random(-positionRange, positionRange)
+	local randomY = math.random(-positionRange, positionRange)
+	local randomZ = math.random(-positionRange, positionRange)
+
+	local randomRotX = math.rad(math.random(-rotationRange, rotationRange))
+	local randomRotY = math.rad(math.random(-rotationRange, rotationRange))
+	local randomRotZ = math.rad(math.random(-rotationRange, rotationRange))
+
+	return CFrame.new(randomX, randomY, randomZ) * CFrame.Angles(randomRotX, randomRotY, randomRotZ)
+end
+
+return RNG
