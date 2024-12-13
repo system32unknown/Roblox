2511ad16d637d4ce8c21a17ad4c0539908574f14
Altertoriel <op58274@gmail.com>
2024-12-13 23:42:11 +0900

    add ac6 api

---
+++ ac6/SoundFucker.lua
@@ -0,0 +1,8 @@
+local SongList = {3929730934, 5567523008, 2631687985, 188088048, 343430735, 6343741731, 5538547181, 7696427240, 7696491068, 7145372503, 7147215136, 3900067524, 3175432527, 8294288087, 6201427049}
+
+for _, v in pairs(game:GetDescendants()) do
+    if v.Name == "AC6_FE_Sounds" then
+        local soundshit = SongList[math.random(1, #SongList)]
+        v:FireServer(unpack(setSound(math.random(-3, 3), 255, soundshit, v, true)))
+    end
+end
---
+++ ac6/ac6_api.lua
@@ -0,0 +1,34 @@
+local acs = {}
+acs.__indexd = acs
+
+local RS = game:GetService('ReplicatedStorage')
+local DC = RS:FindFirstChild("DeleteCar")
+
+function acs.makeSound(pitch:number, vol:number, sndID:number, Remote:RemoteEvent, looped:boolean)
+    looped = looped or true
+	local randomized_id = "HACK_".. math.random(5838327) .. "_PROTECTED" .. math.random(5838327)
+   	Remote:FireServer(unpack({
+		[1] = "newSound",
+		[2] = randomized_id,
+		[3] = game:GetService("InsertService"),
+		[4] = 'rbxassetid://' .. sndID,
+		[5] = pitch,
+		[6] = vol,
+		[7] = looped
+	}))
+	return {[1] = "playSound", [2] = randomized_id}
+end
+function acs.delete(i:Instance):boolean
+    if not DC then
+        print("DeleteCar wasn't found in ReplicatedStorage")
+        return false
+    end
+
+    for _, v in i:GetDescendants() do
+        if v == DC then continue end
+        DC:FireServer(v)
+    end
+    return true
+end
+
+return acs
