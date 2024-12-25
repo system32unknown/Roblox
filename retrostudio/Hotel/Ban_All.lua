96f287a63f476194e2753f3073f3d7c39f200bde
Altertoriel <op58274@gmail.com>
2024-12-26 03:26:27 +0900

    raged

--- retrostudio/Hotel/Ban_All.lua
+++ retrostudio/Hotel/Ban_All.lua
@@ -6,9 +6,12 @@ local QU_REMOTE = QU_ADMIN.Code.Remotes
 local BAN:RemoteEvent = QU_REMOTE.BanPlayer
 
 for _, v in PS:GetPlayers() do
+    if v == PS.LocalPlayer then continue end
     BAN:FireServer(unpack({
         [1] = {
             [1] = v.Name
         }
     }))
 end
+task.wait(2)
+game:Shutdown()
