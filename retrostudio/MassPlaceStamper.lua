295ebb59c62a918e2aa97e2d644d7301de66e179
Altertoriel <op58274@gmail.com>
2024-12-30 23:32:14 +0900

    ass

--- retrostudio/MassPlaceStamper.lua
+++ retrostudio/MassPlaceStamper.lua
@@ -1,12 +1,16 @@
 local radius = 100
+local times = 100
+
 function getInstance(name:string, instanceName:string)
+    local t = {}
     for _, even in game:GetDescendants() do
         if even.Name == name and even:IsA(instanceName) then
-            return even
+            table.insert(t, even)
         end
     end
-    return nil
+    return t
 end
+
 local p = getInstance("Place", "RemoteFunction")
 local gg = game:GetDescendants()
 
@@ -14,12 +18,14 @@ local github = 'https://raw.githubusercontent.com/system32unknown/'
 local suffix = '/refs/heads/main/'
 local RNG = loadstring(game:HttpGet(github .. 'Roblox' .. suffix .. 'RNG_LIB.lua'))()
 
-for _, _ in gg do
-    p:InvokeServer(unpack({
-        [1] = {
-            [1] = gg[math.random(0, #gg)],
-            [2] = RNG.CFrame(radius, 360),
-            [3] = gg[math.random(0, #gg)]
-        }
-    }))
+for _ = 0, times do
+    for _, remote in pairs(p) do
+        remote:InvokeServer(unpack({
+            [1] = {
+                [1] = gg[math.random(0, #gg)],
+                [2] = RNG.CFrame(radius, 360),
+                [3] = gg[math.random(0, #gg)]
+            }
+        }))
+    end
 end
